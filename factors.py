6b5320a4205c81f4373b153f690c7eab49278808
Dottielaleti <laletidottie@gmail.com>
2023-06-02 19:39:01 -0700

    factors

--- factors.py
+++ factors.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""script that factorize as many numbers as possible into a
-   product of two smaller numbers"""
+
+import sys
+from primes import factors
 
 
-def descom_number(num):
-    """Extracts the first prime factor of the number, and displays the
-       output formatted with the 2 respective factors.
-
-    Args:
-        num (int): the number that will be decomposed.
-    Attributes:
-        tmp (int): stores the number that will be decomposed so as not
-                   to lose the value.
-        factor_primo: the number with which we will be dividing
-    """
-    tmp = num
-    factor_primo = 2
-
-    while num > 1:
-        if num % factor_primo == 0:
-            num /= factor_primo
-            print(f"{tmp}={int(num)}*{factor_primo}")
-            break
-        else:
-            factor_primo += 1
-
-
-file = open('tests/test00')
-listlines = file.readlines()
-
-for line in listlines:
-    descom_number(int(line))
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        exit(1)
+    num = []
+    with open(sys.argv[1], 'r') as file:
+        lines = file.readlines()
+    for i in lines:
+        num.append(int(i))
+    for i in num:
+        j = factors(i)
+        if j != 1:
+            if i % 2 == 0:
+                j = 2
+            else:
+                for j in range(3, (i//2) + 1, 2):
+                    if (i % j == 0):
+                        j = i // j
+                        break
+            print("{} = {:d}*{}".format(i, i//j, j))
